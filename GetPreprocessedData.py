83079be3d175caf6d84c497ff133b4e8723b7314
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2021-11-17 22:02:51 +0100

    Fix GetPreprocessedData

--- GetPreprocessedData.py
+++ GetPreprocessedData.py
@@ -7,7 +7,9 @@ def create_parser():
     parser.add_argument('-dir', default='data', help='Directory with gate images', type=str, required=False)
     parser.add_argument('-ts', default=0.7, help='Size of train data', type=float, required=False)
     parser.add_argument('-vs', default=0.2, help='Size of validation data', type=float, required=False)
-    parser.add_argument('-augmentation', default=False, help='Should augmentate data', type=bool, required=False)
+    parser.add_argument('--augmentation', dest='augmentate', help='Augmentate data', action='store_true')
+    parser.add_argument('--no-augmentation', dest='augmentate', help='Do not augmentate data', action='store_false')
+    parser.set_defaults(augmentate=False)
     parser.add_argument('-save_dir', default='preprocessed_data',
                         help='Directory where preprocessed data should be saved', type=str, required=False)
     parser.add_argument('-seed', default=1001, help='Seed', type=int, required=False)
